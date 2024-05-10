a106ed5de25f14a9fa7c004ccd66727ff41d7589
komapotter <36rafts@gmail.com>
2024-05-10 13:14:07 +0900

    Refactored test to use pipes for stdin and stdout handling.

--- cmd/git-aico/main_test.go
+++ cmd/git-aico/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"io"
+	"os"
 	"strings"
 	"testing"
 )
@@ -23,17 +25,36 @@ func TestSelectCommitMessage(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		outBuf := new(bytes.Buffer)
-		reader := strings.NewReader(test.input)
-		writer := outBuf
+		inR, inW, err := os.Pipe()
+		if err != nil {
+			t.Fatalf("os.Pipe() failed with error: %v", err)
+		}
+		os.Stdin = inR
+		defer inR.Close()
+		defer inW.Close()
 
-		selectedMessage, err := selectCommitMessage(suggestions, reader, writer)
+		outR, outW, err := os.Pipe()
+		if err != nil {
+			t.Fatalf("os.Pipe() failed with error: %v", err)
+		}
+		os.Stdout = outW
+		defer outR.Close()
+		defer outW.Close()
+
+		go func() {
+			defer inW.Close()
+			_, _ = inW.Write([]byte(test.input))
+		}()
+		selectedMessage, err := selectCommitMessage(suggestions)
 		if err != nil {
 			t.Errorf("selectCommitMessage returned an unexpected error: %v", err)
 		}
 		if selectedMessage != test.expected {
 			t.Errorf("selectCommitMessage = %q, want %q", selectedMessage, test.expected)
 		}
+		outW.Close()
+		outBuf := new(bytes.Buffer)
+		io.Copy(outBuf, outR)
 		if !strings.Contains(outBuf.String(), "Enter the number of your choice: ") {
 			t.Errorf("Expected user prompt not found in output")
 		}
